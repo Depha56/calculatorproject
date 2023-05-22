97d696bdaf1cf4ef369806e4ce8832970dde5e16
Delphine INGABIRE <82975244+Depha56@users.noreply.github.com>
2023-05-22 12:12:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//date = date.toLocaleString();
+const mylabel = document.getElementById("mylabel");
+update();
+setInterval(update,1000);
+function update(){/**this function is for update seconds */
+     let date = new Date();
+     mylabel.innerHTML = ourdate(date);
+  function ourdate(date){
+     let hours = date.getHours();
+     let minutes = date.getMinutes();
+     let seconds = date.getSeconds();
+     let amorpm = hours>=12 ? "pm" : "am";
+     hours = (hours%12)|| 12;
+     hours = readingZeros(hours);
+     minutes = readingZeros(minutes);
+     seconds = readingZeros(seconds);
+     return `${hours}:${minutes}:${seconds} ${amorpm}`
+   }
+   /**we are going to create function for reading zeros */
+   function readingZeros(time){
+    time = time.toString();
+    return time.length < 2 ? "0" + time : time;
+   }
+}
